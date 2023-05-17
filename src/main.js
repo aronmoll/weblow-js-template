1448012d06b0891d4faa33767a5e75522b05d985
aronmoll <aronmoll6@gmail.com>
2023-05-17 16:34:19 +0200

    slider setup vertical

--- src/main.js
+++ src/main.js
@@ -1,3 +1,40 @@
 import './styles/style.css'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import Lenis from '@studio-freight/lenis'
 
-console.log('Hello')
+gsap.registerPlugin(ScrollTrigger)
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
+
+requestAnimationFrame(raf)
+
+const items = document.querySelectorAll('.slider-item')
+
+items.forEach(item => {
+    gsap.to(item.querySelector('.slider-image'), {
+        yPercent: 50,
+        ease: 'none',
+        scrollTrigger: {
+            trigger: '.selected-work_wrapper',
+            start: 'top bottom',
+            end: 'bottom bottom',
+            scrub: true
+        }
+    })
+})
