f83c1d88963ae88f4f589b8c86c6d601523e2583
aronmoll <aronmoll6@gmail.com>
2023-05-18 11:07:45 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,28 +1,8 @@
-'./styles/style.css'
+import './styles/style.css'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
-import Lenis from '@studio-freight/lenis'
 
 gsap.registerPlugin(ScrollTrigger)
-
-const lenis = new Lenis()
-
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-lenis.on('scroll', ScrollTrigger.update)
-
-gsap.ticker.add((time)=>{
-  lenis.raf(time * 1000)
-})
-
-requestAnimationFrame(raf)
 
 const items = document.querySelectorAll('.slider-item')
 
