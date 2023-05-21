ab6b5aa9aee3638257cb11deb8da5b75182a4e0f
aronmoll <aronmoll6@gmail.com>
2023-05-21 22:07:09 +0200

    debug

--- src/main.js
+++ src/main.js
@@ -48,10 +48,9 @@ for (let i = 0; i < sections.length; i++) {
   gsap.to(wipe, {
     scrollTrigger: {
       trigger: sections[i],
-      start: "top 75%", 
+      start: "top bottom", 
       end: "bottom bottom", 
       scrub: 0.5,
-      markers: true,
     },
     height: "105%", // changed from width to height
   })
