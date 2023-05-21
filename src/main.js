96c441ccbd062a8e982be31f34f53403652c4e58
aronmoll <aronmoll6@gmail.com>
2023-05-21 22:02:48 +0200

    debug

--- src/main.js
+++ src/main.js
@@ -48,7 +48,7 @@ for (let i = 0; i < sections.length; i++) {
   gsap.to(wipe, {
     scrollTrigger: {
       trigger: sections[i],
-      start: "top bottom", 
+      start: "top 75%", 
       end: "bottom bottom", 
       scrub: 0.5,
       markers: true,
